f19b254e3c23e0a1ad2a505282d1adc3c16a689a
Farmacia Artemisia <infopharmamontsrl@gmail.com>
2026-07-02 12:52:14 +0200

    fix view order detail

--- resources/views/auth/order-detail.blade.php
+++ resources/views/auth/order-detail.blade.php
@@ -136,10 +136,10 @@
                                         </div>
                                         <div class="orders-mobile">
                                             @foreach($order->items as $item)
-                                                <div class="order-card">
+                                                <div class="order-detail-content">
                                                     <a href="/shop-single/{{ $item->product()->first()->ean ?? $item->product()->first()->minsan }}">
                                                         <img src="{{asset('/storage-admin/' . $item->product()->first()->image) }}" width="80px">
-                                                        <div class="order-card-details">
+                                                        <div class="order-card-details" style="margin-top: 20px">
                                                             <h5>{{$item->product_name}}</h5>
                                                             <div class="info">Quantità: {{$item->quantity}}</div>
                                                             <div class="info">Prezzo: €{{$item->price}}</div>
