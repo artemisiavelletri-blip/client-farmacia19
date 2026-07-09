ff67407ed6f7342ec8e590900f70b56e1639f13c
Farmacia Artemisia <infopharmamontsrl@gmail.com>
2026-07-09 08:52:36 +0200

    add telegram to exception

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Http;
+use Throwable;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,5 +19,12 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->report(function (Throwable $e) {
+
+            Http::post("https://api.telegram.org/bot8239811344:AAFoIQDAv7VJEFhDa23xDiT0jaRgujzrMm4/sendMessage", [
+                'chat_id' => "-5538285121",
+                'text' => "🚨 ".$e->getMessage()."\n\n".$e->getFile().":".$e->getLine(),
+            ]);
+
+        });
     })->create();
