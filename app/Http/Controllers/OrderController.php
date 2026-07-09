4aff88844106fc96c6d545fdfd49974df9edf935
Farmacia Artemisia <infopharmamontsrl@gmail.com>
2026-07-09 09:36:12 +0200

    fix error token

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -41,6 +41,11 @@ class OrderController extends Controller
         $service = new Track123Service();
         $trackingData = $service->track($order->tracking_number);
 
+        $service2 = new Track123Service();
+        $trackingData2 = $service2->track("1UW0MPZ184705");
+
+        dd($trackingData2);
+
         $tracking = $trackingData['tracking'] ?? null;
         $status = $trackingData['status'] ?? null;
 
